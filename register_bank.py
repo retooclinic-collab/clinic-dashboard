dc13a0b6d8a82d3060587210757972e05ab2bd7d
retooclinic-collab <retooclinic@gmail.com>
2026-08-08 22:59:11 +0900

    CODEF 정식전환: ENV=api 강제

--- register_bank.py
+++ register_bank.py
@@ -20,7 +20,7 @@ import os, json
 from easycodefpy import Codef, ServiceType
 from easycodefpy.util import encrypt_rsa
 
-ENV          = os.environ.get("CODEF_ENV", "demo").lower()
+ENV = "api"  # 2026-08 정식전환 강제 (yml env 무시)
 CLIENT_ID    = os.environ["CODEF_CLIENT_ID"]
 CLIENT_SECRET= os.environ["CODEF_CLIENT_SECRET"]
 PUBLIC_KEY   = os.environ["CODEF_PUBLIC_KEY"]
